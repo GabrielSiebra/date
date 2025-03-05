ddfe5b1a6b95280e83a912ed536d8bb8fa232308
GabrielSiebra <gabrielsiebra2014@gmail.com>
2025-03-05 19:23:19 -0300

    OrderStatus

--- src/main/java/com/gabriel/Program.java
+++ src/main/java/com/gabriel/Program.java
@@ -1,5 +1,6 @@
 package com.gabriel;
 
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,8 +9,11 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
 
 
         sc.close();
